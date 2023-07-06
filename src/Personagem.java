b7ccdad9f11c4d0274ca4def9aed5e8988e368df
Jair Rodrigues <97355011+jairodrigue@users.noreply.github.com>
2023-07-05 23:06:37 -0300

    Parte 3 - Versão final

--- src/Personagem.java
+++ src/Personagem.java
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-=======
-import java.util.ArrayList;
-
->>>>>>> f6a88c02215cedce1d283b78769c315338630342
 public class Personagem {
     String nome;
     int energia;
 
-<<<<<<< HEAD
     public Personagem(String nome, int energia) {
         this.nome = nome;
         this.energia = energia;
@@ -18,24 +12,3 @@ public class Personagem {
     }
 
 }
-=======
-    ArrayList<String> historico = new ArrayList<>();
-
-    public Personagem(String nome, int energia) {
-        this.nome = nome;
-        this.addEnergia(energia);
-    }
-
-    public void addEnergia(int energia) {
-        this.energia = this.energia + energia;
-        historico.add("O personagem " + this.nome + " || Energia: " + this.energia);
-
-    }
-
-    public void historico() {
-        for (String mString : this.historico) {
-            System.out.println(mString);
-        }
-    }
-}
->>>>>>> f6a88c02215cedce1d283b78769c315338630342
